0ba59cadf5d47d4b651a191ae46391b9a16e2b83
Subhashini1984 <59872453+Subhashini1984@users.noreply.github.com>
2024-04-13 22:00:54 -0400

    Add files via upload

---
+++ SearchPage/cypress/e2e/1-getting-started/searchPage.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('search for a product', () => {   
+     it('search for a product', ()=> 
+   {
+      cy.visit('https://www.supplyhouse.com/', {failOnStatusCode: false})
+      //cy.contains('Continue').click()
+      //cy.get('button').check()
+      //cy.url().should('include', '/next-page')
+      
+      cy.wait(4000); 
+      
+      cy.get('#react-header-search-input').type('taco')
+      cy.wait(4000); 
+      cy.xpath("//div[@class='Box-sc-1z9git-0 jvLuNd']/div//span").then( items => {
+      //expect(items[1]).to.have.css('font-weight','400')
+     
+      expect(items[1]).to.have.css('font-weight','normal')
+      expect(items[1]).to.have.css('-webkit-text-fill-color','rgb(52, 52, 52)')
+      
+    })
+  
+    cy.xpath("//div[@class='Box-sc-1z9git-0 jvLuNd']/div").then( items => 
+    {
+    expect(items[1]).to.have.css('font-weight', 'bold')
+    expect(items[1]).to.have.css('-webkit-text-fill-color','rgb(0, 0, 0)')
+
+    })
+  })
+})
+    
